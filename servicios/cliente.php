82f86232d54547cc7409b41ef3e79124871eefa8
DiegoPaul-2001 <diegoquimbit2001@gmail.com>
2021-06-22 19:23:28 -0500

    consulta no vale

--- servicios/cliente.php
+++ servicios/cliente.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Listado Clientes</title>
+</head>
+<body>
 <?php  
     $nombre = $_GET["nombre"];
     $cedula = $_GET["cedula"];
@@ -19,3 +32,25 @@
         echo $e->getMessage().PHP_EOL;
     } 
 ?>
+
+    <?php    
+    try {
+        $cliente = new SoapClient('http://localhost/TiendaS/servicios/wdsl.xml');
+        $r = $cliente->consultarTodos();
+        echo "<table class='table table-light'>";
+        echo "
+            <tr class='table-dark'>
+                <td>ID</td>
+                <td>NOMBRE</td>
+                <td>APELLIDO</td>
+                <td>EDAD</td>
+            </tr>
+            ";
+        echo ($r);
+        echo "</table>";
+    } catch (SoapFault $e) {
+        echo $e->getMessage().PHP_EOL;
+    } 
+    ?>
+</body>
+</html>
--- servicios/server.php
+++ servicios/server.php
@@ -1,9 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 class miClase{
     function insertar($nombre,$cedula,$usuario,$contra,$email,$telefono,$direccion):int{
         include("../config/conexion.php");
         return $db->query("insert into clientes values ('','$nombre','$cedula','$usuario','$contra','$email','$telefono','$direccion');");
+    }
+    function consultarTodos():string{
+        include "../conectar/conectar.php";
+        $r = $db->query("select * from clientes;");
+        $html="";
+        while($row = $r->fetch_assoc()){
+            $html="
+            <tbody>
+            <tr>
+            <td>".$row['id']."</td>
+            <td>".$row['nombre']."</td>
+            <td>".$row['apellido']."</td>
+            <td>".$row['edad']."</td>
+            <td><button onclick='actualizar('".$row['id']."')' value='Actualizar'></td>
+            </tr></tbody>";
+        }
+        return $html;
+
     }
 }
 try {
@@ -14,4 +45,6 @@ try {
     print $e->faultstring;
 }
 
-?>
+?>            
+</body>
+</html>
